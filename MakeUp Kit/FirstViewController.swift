369654bca6c0cdbd7dbac46a000224f09f250237
Alina Huk <alina.huk151@gmail.com>
2020-05-29 21:10:34 +0300

    first commit

---
+++ MakeUp Kit/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MakeUp Kit
+//
+//  Created by Alina Huk on 29.05.2020.
+//  Copyright © 2020 Alina Huk. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MakeUp Kit/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MakeUp Kit
+//
+//  Created by Alina Huk on 29.05.2020.
+//  Copyright © 2020 Alina Huk. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
